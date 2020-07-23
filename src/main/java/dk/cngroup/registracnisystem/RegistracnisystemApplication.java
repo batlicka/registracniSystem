03ab3d8e2d03e3149d437b71b3740d95ce54d634
v_vagunda <vagunda@cngroup.dk>
2020-07-23 12:45:27 +0200

    foundation commit

---
+++ src/main/java/dk/cngroup/registracnisystem/RegistracnisystemApplication.java
@@ -0,0 +1,14 @@
+package dk.cngroup.registracnisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistracnisystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistracnisystemApplication.class, args);
+	}
+	//note
+
+}
---
+++ src/test/java/dk/cngroup/registracnisystem/RegistracnisystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.cngroup.registracnisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistracnisystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
